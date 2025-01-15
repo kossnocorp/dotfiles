dfd9e517068607d3fbac97f5adb9a4bb0c753c6b
Sasha Koss <koss@nocorp.me>
2025-01-16 06:14:39 +0800

    Add Git aliases

--- configs/zsh/config.zsh
+++ configs/zsh/config.zsh
@@ -7,3 +7,12 @@ fi
 # Add scripts to PATH
 SCRIPTS_PATH="$HOME/src/dotfiles/scripts"
 export PATH="$SCRIPTS_PATH/$OS:$SCRIPTS_PATH/common:$PATH"
+
+#region Aliases
+
+# Git
+alias glr="git pull --rebase" 
+alias grh="git reset --hard" 
+alias gcob="git checkout -b"
+
+#endregion
