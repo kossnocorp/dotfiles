b8e1c42676bb908e786a5cdbecf2eade0135a8b7
Sasha Koss <koss@nocorp.me>
2025-01-20 08:58:32 +0800

    Add more aliases to zsh config

--- configs/zsh/config.zsh
+++ configs/zsh/config.zsh
@@ -1,3 +1,6 @@
+#region Variables
+
+# Assign OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export OS="macos"
 elif [[ "$(uname)" == "Linux" ]]; then
@@ -8,12 +11,17 @@ fi
 SCRIPTS_PATH="$HOME/src/dotfiles/scripts"
 export PATH="$SCRIPTS_PATH/$OS:$SCRIPTS_PATH/common:$PATH"
 
+#endregion
+
 #region Aliases
 
 # Git
 alias glr="git pull --rebase" 
 alias grh="git reset --hard" 
 alias gcob="git checkout -b"
+
+# GitHub CLI
+alias ghco="gh pr checkout"
 
 # nw
 alias n="nw"
