3a5d0d9b39a2f6165f07f49bce9978b53296ae2b
Sasha Koss <koss@nocorp.me>
2025-08-08 04:43:20 +0000

    Complete base WezTerm setup

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,6 +6,17 @@ local config = wezterm.config_builder()
 if string.find(wezterm.target_triple, 'windows') then
   -- Use Powershell
   config.default_prog = { 'pwsh.exe' }
+
+  -- GUI
+  config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 end
+
+-- GUI
+config.initial_cols = 120
+config.initial_rows = 30
+
+-- Font
+config.font = wezterm.font('IosevkaTerm NF')
+config.harfbuzz_features = { 'calt', 'ss01' }
 
 return config
