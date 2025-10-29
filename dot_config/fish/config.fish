59fad5127b3e8a42c6406e8f133314805f444221
Sasha Koss <koss@nocorp.me>
2025-10-29 09:41:40 +0800

    Fix 1Password's SSH_AUTH_SOCK

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -52,7 +52,7 @@ end
 # 1Password
 
 if test (uname) = Darwin
-  set -gx SSH_AUTH_SOCK "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+  set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 end
 
 # Aliases
