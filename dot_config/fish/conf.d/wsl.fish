c3b8169d5762ac60d4a42f61e8a9dbb40fa48e95
Sasha Koss <koss@nocorp.me>
2026-04-21 13:08:50 +0800

    Adjust ROCDXG setting, set up Ollama on WSL

--- dot_config/fish/conf.d/wsl.fish
+++ dot_config/fish/conf.d/wsl.fish
@@ -4,4 +4,9 @@ if not string match -rq 'microsoft|wsl' -- (uname -r)
 end
 
 # AMD ROCDXG library (https://github.com/ROCm/librocdxg/)
-set HSA_ENABLE_DXG_DETECTION 1
+set -gx HSA_ENABLE_DXG_DETECTION 1
+
+# Ollama
+# NOTE: `librocdxg` is not supported yet by Ollama, remove it when needed: https://github.com/ollama/ollama/issues/5275
+set -l WIN_HOST (ip route show | grep -i default | awk '{ print $3 }')
+set -gx OLLAMA_HOST "http://$WIN_HOST:11434"
