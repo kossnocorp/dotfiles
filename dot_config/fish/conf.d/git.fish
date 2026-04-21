1da68e68625d2191a6370a9a8ce1cc3510822f74
Sasha Koss <koss@nocorp.me>
2026-04-21 10:16:57 +0800

    Move git fish aliases to a separate config, add `gfa`

---
+++ dot_config/fish/conf.d/git.fish
@@ -0,0 +1,50 @@
+# Aliases
+
+# status
+alias gs "git status"
+
+# push
+alias gp "git push"
+
+# pull
+alias glr "git pull --rebase"
+
+# fetch
+alias gfa "git fetch --all"
+
+# commit
+alias gc "git commit"
+alias gcm "git commit -m"
+alias gca "git commit --amend"
+
+# checkout
+alias gco "git checkout"
+alias gcob "git checkout -b"
+
+# add
+alias ga "git add"
+alias gaa "git add ."
+
+# reset
+alias grsh "git reset --hard"
+alias grs1 "git reset HEAD~1"
+
+# diff
+alias gd "git diff"
+alias gds "git diff --staged"
+
+# rebase
+alias grb "git rebase"
+alias grb2 "git rebase -i HEAD~2"
+alias grb3 "git rebase -i HEAD~3"
+alias grb4 "git rebase -i HEAD~4"
+alias grb5 "git rebase -i HEAD~5"
+
+# branch
+alias gb "git branch"
+alias gbr "git branch -r"
+alias gbd "git branch -D"
+
+# stash
+alias gsh "git stash"
+alias gshp "git stash pop"
